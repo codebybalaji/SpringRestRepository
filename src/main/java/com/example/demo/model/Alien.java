7b779b1b2768e92540b1cc4c2ad1c94e93c15029
balaji-sirius <balaji.soundarrajan@siriuscom.com>
2021-02-15 15:43:45 +0530

    SPringBoot without rest controller

---
+++ src/main/java/com/example/demo/model/Alien.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Alien {
+	@Id
+	@GeneratedValue
+	private int aid;
+	private String aName;
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getaName() {
+		return aName;
+	}
+	public void setaName(String aName) {
+		this.aName = aName;
+	}
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", aName=" + aName + "]";
+	}
+	
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into alien values(1,'balaji');
+insert into alien values(2,'ragul');
+insert into alien values(3,'Deva');
+insert into alien values(4,'sundar');
+insert into alien values(5,'thatha');
+insert into alien values(6,'janaki');
